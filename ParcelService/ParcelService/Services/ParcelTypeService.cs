409fa06f05e751c2bc632d56ebbb58cca6131c36
Ben Roberts <bendando@hotmail.co.uk>
2023-04-20 11:24:29 +0100

    Task1: Success tests passing for caculating parcel size

--- ParcelService/ParcelService/Services/ParcelTypeService.cs
+++ ParcelService/ParcelService/Services/ParcelTypeService.cs
@@ -7,7 +7,14 @@ namespace ParcelService.Services
     {
         public ParcelType GetParcelType(Parcel parcel)
         {
-            throw new NotImplementedException();
+            if (parcel.Width < 10 && parcel.Height < 10 && parcel.Depth < 10)
+                return ParcelType.Small;
+            if (parcel.Width < 50 && parcel.Height < 50 && parcel.Depth < 50)
+                return ParcelType.Medium;
+            if (parcel.Width < 100 && parcel.Height < 100 && parcel.Depth < 100)
+                return ParcelType.Large;
+            else
+                return ParcelType.XL;
         }
     }
 }
